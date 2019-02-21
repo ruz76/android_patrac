00b5184ccb2a95fd8aa68b3f668dfb8126089ba6
ruz76 <jan.ruzicka.vsb@gmail.com>
2019-02-21 13:53:24 +0100

    Testing of sleeping.

--- Application/src/main/java/cz/vsb/gis/ruz76/patrac/android/receivers/PositionReceiver.java
+++ Application/src/main/java/cz/vsb/gis/ruz76/patrac/android/receivers/PositionReceiver.java
@@ -19,17 +19,35 @@ import cz.vsb.gis.ruz76.patrac.android.helpers.LogHelper;
 
 public class PositionReceiver extends BroadcastReceiver implements GetRequestUpdate {
 
+    private String url = null;
+
     @Override
     public void onReceive(Context context, Intent intent) {
         //Toast.makeText(context, "I'm running", Toast.LENGTH_SHORT).show();
+        if (url == null) {
+            url = Status.endPoint;
+        }
         LogHelper.i("PositionReceiver", String.valueOf(new Date()));
         GetRequest getRequest = new GetRequest();
         getRequest.setActivity(this);
-        getRequest.execute(Status.endPoint + "date=" + new Date());
+        if (url == null) {
+            LogHelper.i("PositionReceiver", "URL is null");
+            getRequest.execute("http://gisak.vsb.cz/patrac/mserver.php?date=" + new Date());
+        } else {
+            getRequest.execute(url + "date=" + new Date());
+        }
     }
 
     @Override
     public void processResponse(String result) {
+        LogHelper.i("PositionReceiverResponse", result);
+    }
 
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
     }
 }
